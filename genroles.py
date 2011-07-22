d148d19d8ddbcc32f867e2c159ad384b444011bf
Brandon R. Stoner <monokrome@monokro.me>
2011-07-22 10:25:09 -0600

    Added initial role generation.

---
+++ genroles.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import email.mime.text
+import email.mime.multipart
+import glob
+import os
+
+def project_path(identifier):
+	return os.path.abspath(os.path.join('.', identifier))
+
+dist_dir = project_path('dist')
+roles_dir = project_path('roles')
+
+global_files = []
+roles = {}
+
+# Iterate through list of all absolute paths in the files directory
+for filename in glob.iglob(os.path.join(roles_dir, '*')):
+
+	if os.path.isdir(filename):
+		roles[os.path.basename(filename)] = filename
+	else:
+		current_file = open(filename, 'r')
+		data = current_file.read()
+
+		global_files.append(email.mime.text.MIMEText(data))
+
+# Create a directory to store our file output in
+if not os.path.exists(dist_dir):
+	os.makedirs(dist_dir)
+
+elif not os.path.isdir(dist_dir):
+	raise ValueError('{0} must be a directory.'.format(dist_dir))
+
+# Generate output for each role
+for role in roles:
+
+	role_dir = roles[role]
+	role_filename = '{0}{1}'.format(role, '.out')
+
+	role_output = open(os.path.join(dist_dir, role_filename), 'w')
+	
+	# Create a multi-part MIME file, which will be used to store
+	# all of the related files for this role.
+	mime_output = email.mime.multipart.MIMEMultipart()
+
+	# Include all "global" requirements.
+	for mime_file in global_files:
+		mime_output.attach(mime_file)
+
+	for filename in glob.iglob(os.path.join(role_dir, '*')):
+		current_file = open(filename)
+		current_mime = email.mime.text.MIMEText(current_file.read())
+		current_file.close()
+
+		current_mime._headers = []
+
+		mime_output.attach(current_mime)
+
+	role_output.write(str(mime_output))
+	role_output.close()
